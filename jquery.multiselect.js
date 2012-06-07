97b9d19c5268490348eca733ad05a47c10ebf2f1
Kantoor Weesp Ruud <Kantoor Weesp Ruud@192.168.1.134>
2012-06-07 09:36:35 +0200

    Added disabled and showOption.

--- jquery.multiselect.js
+++ jquery.multiselect.js
@@ -25,14 +25,15 @@
 
 	$.fn.multiselect = function(_options) {
 		var options = {
-			width:		undefined		// same
-		,	height:		undefined		// same
+			width:			undefined	// undefined, 'fit', numeric or css-unit
+		,	height:			undefined	// undefined, numeric or css-unit
+		,	showOption:		undefined	// callback(text, value, index) for each option
 		}
 
 		return this.each( function() {
 			var element	= this;
 
-			$(element).hide();
+			//$(element).hide();
 
 			if (_options) {
 				$.extend(options, _options);
@@ -47,25 +48,36 @@
 						: options.height?				options.height
 						:								$(element).height()+'px'
 						;
-			var select = $('<div tabindex="0" class="multiselect-select" style="height:'+height+';width:'+width+';"/>').insertAfter(element);
+			var select = $('<div tabindex="0" class="multiselect-select" style="height:'+height+';width:'+width+';"/>')
+							.insertAfter(element);
 
-			$('option', element).each( function(_index, _option) {
+			$('option', element).each( function(index, option) {
 				var handleClick = function(event) {
 					event.stopPropagation();
-					set_state(!item.hasClass('multiselect-selected'), $(_option), checkbox, item);
 
-					$(_option)
-					.trigger('change');
+					if (!$(option).is(':disabled')) {
+						set_state(!item.hasClass('multiselect-selected'), $(option), checkbox, item);
+
+						$(option).trigger('change');
+					}
 				};
 
-				var state		= $(_option).is(':selected');
-				var item		= $('<div class="multiselect-option'+(state ? ' multiselect-selected' : '')+'"/>')
+				var selected	= $(option).is(':selected');
+				var disabled	= $(option).is(':disabled');
+
+				var item		= $('<div class="multiselect-option'+(selected ? ' multiselect-selected' : '')+(disabled ? ' multiselect-disabled' : '')+'"/>')
 									.click(handleClick)
 									.appendTo(select);
-				var checkbox	= $('<input type="checkbox"'+(state ? ' checked="checked"' : '')+'/>')
+
+				var checkbox	= $('<input type="checkbox"'+(selected ? ' checked="checked"' : '')+(disabled ? ' disabled="disabled"' : '')+'/>')
 									.click(handleClick)
 									.appendTo(item);
-				var $label		= $('<span>'+$(_option).text()+'</span>')
+
+				var text		= $(option).text();
+				if (typeof options.showOption == 'function') {
+					text = options.showOption(text, $(option).val(), index);
+				}
+				var label		= $('<span>'+text+'</span>')
 									.appendTo(item);
 			});
 		});
